ffef0d888d40e3b4abd71731ad78595616d4090f
Rangeet Pan <23088883+rangeetpan@users.noreply.github.com>
2025-04-09 10:17:21 -0700

    Update models.py

--- cldk/analysis/commons/treesitter/models.py
+++ cldk/analysis/commons/treesitter/models.py
@@ -47,10 +47,8 @@ class Captures:
         node: Node
         name: str
 
-    def __init__(self, captures: Dict[str, List[Node]]):
-        self.captures = []
-        for capture_name, captures in captures.items():
-            self.captures = [self.Capture(node=node, name=capture_name) for node in captures]
+    def __init__(self, captures: List[Tuple[Node, str]]):         
+        self.captures = [self.Capture(node=node, name=text) for node, text in captures]
 
     def __getitem__(self, index: int) -> Capture:
         """Get the capture at the specified index.
