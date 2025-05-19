329f156e097db2d117d45340ac1689275e3ac58e
Saurabh Sinha <sinha108@gmail.com>
2025-05-19 13:22:13 -0400

    Added test case and fixture for source analysis

--- tests/analysis/java/test_java_analysis.py
+++ tests/analysis/java/test_java_analysis.py
@@ -50,6 +50,24 @@ def test_get_symbol_table_is_not_null(test_fixture, analysis_json):
         )
         assert analysis.get_symbol_table() is not None
 
+def test_get_symbol_table_source_code(java_code):
+    """Should return a symbol table for source analysis with expected class/method count"""
+
+    # Initialize the CLDK object with the project directory, language, and analysis_backend
+    cldk = CLDK(language="java")
+    analysis = cldk.analysis(
+        source_code=java_code,
+        analysis_backend_path=None,
+        eager=True,
+        analysis_level=AnalysisLevel.symbol_table,
+    )
+
+    # assert on expected class name and method count in the symbol table
+    expected_class_name = "com.acme.modres.WeatherServlet"
+    assert analysis.get_symbol_table() is not None
+    assert len(analysis.get_symbol_table().keys()) == 1
+    assert expected_class_name in analysis.get_methods().keys()
+    assert len(analysis.get_methods().get(expected_class_name).keys()) == 9
 
 def test_get_imports(test_fixture, analysis_json):
     """Should return NotImplemented for get_imports()"""
